a508221df1b9f32274ece5f638eed0b2edbc3de0
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-20 12:44:10 -0500

    Layout of project show

--- app/javascript/app/components/projects/project.js
+++ app/javascript/app/components/projects/project.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import glamorous from 'glamorous';
 import { Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const ProjectContainer = glamorous.div({
   width: '100%',
   minHeight: '110px',
-  border: '1px solid rgba(52, 73, 94, 0.4)',
   borderRadius: '2px',
   backgroundColor: '#fff',
   padding: '8px',
@@ -28,7 +28,7 @@ const Img = glamorous.img({
 
 const ProjectInfo = glamorous.div({
   width: 'calc(100% - 100px)',
-  height: '80px',
+  minHeight: '80px',
   padding: '0 10px'
 });
 
@@ -38,18 +38,20 @@ const ProjectName = glamorous.h1({
 })
 
 const Project = (props) => (
-  <ProjectContainer>
-    <ProjectImg>
-      <Img src="http://lorempixel.com/output/business-q-c-200-200-10.jpg" />
-    </ProjectImg>
-    <ProjectInfo>
-      <ProjectName>Company Name 1</ProjectName>
-      <p className="text-12 margin-top-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <p className="text-12 margin-none"><span className="text-green">Tecnologias:</span>: HTML5/CSS3/SASS/JAVASCRIPT/REACT/RUBY</p>
-      <p className="text-12 margin-none"><span className="text-red">Nº de miembros:</span> 2/6 </p>
-      <p className="text-12 margin-none"><span className="text-orange">Duración:</span> 3 meses (20/06/2017 - 20/09/2017)</p>
-    </ProjectInfo>
-  </ProjectContainer>
+  <Link to={`/projects/${'2'}`} className="project" >
+    <ProjectContainer>
+      <ProjectImg>
+        <Img src="http://lorempixel.com/output/business-q-c-200-200-10.jpg" />
+      </ProjectImg>
+      <ProjectInfo>
+        <ProjectName className="text-midnight">Company Name 1</ProjectName>
+        <p className="text-12 margin-top-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p className="text-12 margin-none"><span className="text-bold">Tecnologias:</span>: HTML5/CSS3/SASS/JAVASCRIPT/REACT/RUBY</p>
+        <p className="text-12 margin-none"><span className="text-bold">Nº de miembros:</span> 2/6 </p>
+        <p className="text-12 margin-none"><span className="text-bold">Duración:</span> 3 meses (20/06/2017 - 20/09/2017)</p>
+      </ProjectInfo>
+    </ProjectContainer>
+  </Link>
 );
 
 export default Project;
