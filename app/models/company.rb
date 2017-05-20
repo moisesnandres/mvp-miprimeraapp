8173080fb1a8340dff702ed183b5394855e5cac8
Fabian Pierre Peña Jacobo <f.pena.jacobo@gmail.com>
2017-05-20 11:24:40 -0500

    ADD: validations to user create and company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,10 @@
 class Company < ApplicationRecord
     belongs_to :category    
     validates :email, uniqueness: {message: 'Email debe ser único'}
+    validates :email, presence: {message: 'Email no puede estar en blanco'}
+    validates :password, presence: {message: 'Contraseña no puede estar en blanco'}
+    validates :ruc, presence: {message: 'Email no puede estar en blanco'}
+    validates :ubication, presence: {message: 'Contraseña no puede estar en blanco'}
+    validates :description, presence: {message: 'Email no puede estar en blanco'}
+    validates :sector, presence: {message: 'Contraseña no puede estar en blanco'}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     belongs_to :type_user, optional: true
-    validates :email, uniqueness: {message: 'Email debe ser único'}
+    validates :email, uniqueness: {message: 'Email ya existe'}
+    validates :email, presence: {message: 'Email no puede estar en blanco'}
+    validates :password, presence: {message: 'Contraseña no puede estar en blanco'}
 end
