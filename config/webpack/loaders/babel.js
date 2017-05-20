5b9fa221a5906f6409a3bc566efd22b2b4392344
Moisés Ñañez <moisesnandres@gmail.com>
2017-05-20 10:10:22 -0500

    Dummy files

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -1,11 +1,5 @@
 module.exports = {
   test: /\.js(\.erb)?$/,
   exclude: /node_modules/,
-  loader: 'babel-loader',
-  options: {
-    presets: [
-      ['env', { modules: false }],
-      'react', 'es2015', 'stage-0', 'stage-1'
-    ]
-  }
+  loader: 'babel-loader'
 }
