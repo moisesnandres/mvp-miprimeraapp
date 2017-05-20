11a736eadad94ed22b9fdcac0c174b3ec016f993
Fabian Pierre Peña Jacobo <f.pena.jacobo@gmail.com>
2017-05-20 10:46:05 -0500

    ADD: login service user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(email: 'fabian@rrr.com', password: '123123123')
+user1 = User.create(email: 'luis@rrr.com', password: '123123123')
